9e8358a59d3f1ca1429f3f39d55f8b8a3e3f22a2
Jesse Bloomster <jab83@wilkes.cs.calvin.edu>
2016-09-21 13:24:27 -0400

    lab03

---
+++ lab03/public/msg.js
@@ -0,0 +1,8 @@
+/* 
+ * msg.js is a JavaScript file that will log a message to the console.
+ * 
+ * author: Jesse Bloomster
+ * date: September 21, 2016
+ */
+
+console.log("Hello, from JavaScript!");
